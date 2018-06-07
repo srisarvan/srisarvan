0a1622962d55a7ec6e3573a0539b4fa55e245a30
P9144626 <Saravanan.Narayanan@mnscorpdev.net>
2018-06-07 11:06:47 +0100

    minor_change_3

--- Sa/src/main/java/Sg/Sa/App.java
+++ Sa/src/main/java/Sg/Sa/App.java
@@ -12,7 +12,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hi From .. Hello World!" );
+        System.out.println( "Hi From .. Hello World!!!" );
     	SpringApplication.run(App.class, args);
     }
 }
